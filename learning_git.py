d063a81d4a9644f40cbd7935f24b9853b2de52a4
Sharada1415 <ramyar.alpha@gmail.com>
2026-02-06 19:39:24 +0530

    added prime_number attribute

--- learning_git.py
+++ learning_git.py
@@ -7,13 +7,18 @@ class Numbers:
             else:
                 print(f"{num} is odd")
 
+    def prime_numbers(self):
+        for i in range(2, 50):
+            for j in range(2, i):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+
 
 num = Numbers()
 num.even_odd(10, 20)
-
-
-
-
+num.prime_numbers()
 
 
 
